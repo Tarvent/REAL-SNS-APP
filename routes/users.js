62f5ad7932aecb488973558b6c12d3d3d3d0a895
Tarvent <you@example.com>
2025-05-06 00:31:03 +0900

    first commit

--- routes/users.js
+++ routes/users.js
@@ -53,6 +53,7 @@ router.put("/:id/follow", async (req, res) => {
         try{
             const user = await User.findById(req.params.id);
             const currentUser = await User.findById(req.body.userId)
+            
             if(!user.followers.includes(req.body.userId)){
                await user.updateOne({
                  $push: { followers: req.body.userId },
@@ -72,7 +73,32 @@ router.put("/:id/follow", async (req, res) => {
     }
 })
 
-
+// unfollow user
+router.put("/:id/unfollow", async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId)
+            // Check if the user is already existing in the followers array
+            // If yes, then remove the user from the followers array
+            if(user.followers.includes(req.body.userId)){
+               await user.updateOne({
+                 $pull: { followers: req.body.userId },
+               });
+                await currentUser.updateOne({
+                  $pull: { followings: req.params.id },
+                });
+                return res.status(200).json("User has been unfollowed!");
+            }else{
+                return res.status(403).json("You don't follow this user!");
+            }
+        }catch(err){
+            return res.status(500).json(err);
+        }
+    }else{
+        return res.status(500).json("You can't unfollow yourself!");
+    }
+})
 
 
 
